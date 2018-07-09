9f90b4eafa6111845055707571732cd12a17682e
Shrikant <shrikantmamgain123@gmail.com>
2018-07-09 22:18:56 +0530

    1. Set-up Networking Classes and POJO Classes to make API Calls 2. build.gradle file modified with dependencies 3. Recyclerview and toolbar added in XML files 4. Manifest file modified in order to hide windows action bar

---
+++ app/src/main/java/com/org/demowipro/NetworkingService/ImageAPIInterface.java
@@ -0,0 +1,13 @@
+package com.org.demowipro.NetworkingService;
+
+import com.org.demowipro.RequestPOJO.RowContentInfo;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ImageAPIInterface {
+
+    @GET("s/2iodh4vg0eortkl/facts.json")
+    Call<RowContentInfo> getJSON();
+
+}
---
+++ app/src/main/java/com/org/demowipro/RequestPOJO/RowContentInfo.java
@@ -0,0 +1,32 @@
+package com.org.demowipro.RequestPOJO;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class RowContentInfo {
+
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("rows")
+    @Expose
+    private List<RowDescription> rows = null;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<RowDescription> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<RowDescription> rows) {
+        this.rows = rows;
+    }
+}
