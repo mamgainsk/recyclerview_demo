79f8ab434e396a02590f60177a6f3f811d62f510
Shrikant <shrikantmamgain123@gmail.com>
2018-07-10 14:46:33 +0530

    1. Progress bar added to the layout in order to improve UI 2. Internet Connection Check and Empty List check implemented 3. Changes in XML to improve UI

--- app/src/main/java/com/org/demowipro/MainActivity.java
+++ app/src/main/java/com/org/demowipro/MainActivity.java
@@ -1,12 +1,18 @@
 package com.org.demowipro;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.org.demowipro.adapter.RecyclerViewAdapter;
 import com.org.demowipro.networking_service.APICallback;
@@ -27,14 +33,15 @@ public class MainActivity extends AppCompatActivity {
     private List<RowDescription> rowDescriptions;
     private RecyclerViewAdapter recyclerViewAdapter;
     private LinearLayoutManager linearLayoutManager;
-    private AppCompatTextView toolbarTitle;
     private SwipeRefreshLayout swipeRefreshLayout;
+    private Toolbar toolbar;
+    private ProgressBar progressBar;
+    private AppCompatTextView msgTextView;
 
     private SwipeRefreshLayout.OnRefreshListener refreshRecyclerViewListener = new SwipeRefreshLayout.OnRefreshListener() {
         @Override
         public void onRefresh() {
-
-            getDataFromAPI();
+            getDataFromAPI(true);
         }
     };
 
@@ -44,12 +51,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.staggered_list);
-        toolbarTitle = findViewById(R.id.title_toolbar);
+        toolbar = findViewById(R.id.toolbar);
         swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
+        progressBar = findViewById(R.id.progress_bar);
+        msgTextView = findViewById(R.id.msg_textview);
 
         swipeRefreshLayout.setOnRefreshListener(refreshRecyclerViewListener);
 
-        getDataFromAPI();
+
+        if (isNetworkAvailable()) {
+            getDataFromAPI(false);
+        } else {
+            showHideView(true);
+            msgTextView.setText("Please Check Internet Connection!");
+        }
+
 
     }
 
@@ -58,7 +74,10 @@ public class MainActivity extends AppCompatActivity {
      * Use of retrofit to consume JSON API and send data to Adapter class
      **/
 
-    private void getDataFromAPI() {
+    private void getDataFromAPI(boolean isForceRefresh) {
+        showHideView(false);
+        if (!isForceRefresh)
+            progressBar.setVisibility(View.VISIBLE);
 
         NetworkingService.fetchResponse(new APICallback() {
             @Override
@@ -68,23 +87,55 @@ public class MainActivity extends AppCompatActivity {
 
                 if (rowContentInfo != null) {
                     rowDescriptions = rowContentInfo.getRows();
-                    toolbarTitle.setText(rowContentInfo.getTitle());
+                    toolbar.setTitle(rowContentInfo.getTitle());
                     recyclerViewAdapter = new RecyclerViewAdapter(rowDescriptions);
                     linearLayoutManager = new LinearLayoutManager(getApplicationContext());
                     recyclerView.setHasFixedSize(true);
                     recyclerView.setLayoutManager(linearLayoutManager);
                     recyclerView.setAdapter(recyclerViewAdapter);
+                    showHideView(false);
+
+                } else {
+                    msgTextView.setText("No Data Available...");
+                    showHideView(true);
+
+
                 }
                 swipeRefreshLayout.setRefreshing(false);
+                progressBar.setVisibility(View.GONE);
             }
 
             @Override
             public void onFailure(Call<?> call, Throwable t, int requestCode) {
                 Log.e(TAG, "Failed : " + t.getLocalizedMessage());
+                msgTextView.setText("Failed To Retrieve Data...");
                 swipeRefreshLayout.setRefreshing(false);
+                progressBar.setVisibility(View.GONE);
+                showHideView(true);
+
+
             }
         }, 1);
-        // Use of HttpLoggingInterceptor is redundant: written here  to show logs
+    }
 
+    //---------------------- Internet Connection Check ---------------------------
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+
+    //------------------------ If List Is Empty ----------------------------------
+    private void showHideView(boolean isListEmpty) {
+        if (isListEmpty) {
+            recyclerView.setVisibility(View.GONE);
+            msgTextView.setVisibility(View.VISIBLE);
+
+        } else {
+            recyclerView.setVisibility(View.VISIBLE);
+            msgTextView.setVisibility(View.GONE);
+        }
     }
 }
